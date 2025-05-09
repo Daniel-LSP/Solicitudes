2124dcd649d6cd6b6703101f4ccd39e0c46398bb
Daniel-LSP <gcinovation12@gmail.com>
2025-05-09 13:39:35 -0500

    estilos

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, make_response
 
 
 app = Flask (__name__)
@@ -24,23 +24,45 @@ def ruta_consulta():
     return f"Se esta consultando el producto {producto} y la talla {talla}"
 
 #Ruta para capturar datos por el cuerpo de solicitud para el body
+listado=[]
 @app.route("/registro", methods=["GET"])
 def ruta_registro():
-    return render_template("formulario.html")
+    #listado=[{"nombre": "Lewin", "email":"lewindcg@gmail.com"}]
+    return render_template("formulario.html", listado=listado)
 
 @app.route("/registro", methods=["POST"])
 def procesar_registro():
 
     nombre=request.form.get("nombre")
     correo=request.form.get("correo")
-
+    estudiantes={"nombre":nombre, "correo":correo}
+    listado.append(estudiantes)
+    #print (nombre)
     return f"El estudiante a registrar es {nombre} y el correo a registrar es {correo}"
 
+#PARAMETROS EN LA RUTA
 
+@app.route("/estudiantes/<string:area>/<int:grupo>")
+def mostrar_estudiantes(area, grupo):
+    return f"El programa de formacion consultado es {area} y el grupo consultado es {grupo}"
 
+#SOLICITUD TIPO 4 ENCABEZADOS 
+@app.route("/ver-headers")
+def ver_headers():
+    agente_usuario = request.headers.get('User-Agent')
+    return f"Tu navegador es: {agente_usuario}"
 
+#GESTION DE LAS COOKIES
+@app.route('/crear-cookie')
+def crear_cookie():
+    respuesta = make_response("Cookie creada!")#ES UN MENSAJE EN NAVEGADOR
+    respuesta.set_cookie('usuario_logueado', 'true', max_age=60*60*24, httponly=True)
+    return respuesta
 
-
+@app.route('/leer-cookie')
+def leer_cookie():
+    valor = request.cookies.get('usuario_logueado')
+    return f"Valor de la cookie: {valor}"
 
 if __name__ == "__main__": 
     app.run(debug=True)
